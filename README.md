bb0373c49756798165eee7730fe02f0351b1239b
mstam <m.stamoulos@gmail.com>
2020-04-09 00:22:05 +0100

    README update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # web-crawler
+
+##Description
+
+Simple Java Web Crawler. You can provide an entry point at WebCrawlerApplication.java of a domain and the application will start crawling into the domain displaying  Internal Links, Enternal links and resources for each of the page.
+
+##Compile the app 
+mvn clean complile
+
+
+##Build the app 
+mvn clean install
+
+
+##To run the app 
+
+mvn exec:java -Dexec.mainClass=org.wiprodigital.webcrawler.main.WebCrawlerApplication
--- src/main/java/org/wiprodigital/webcrawler/consumer/ResultPrinter.java
+++ src/main/java/org/wiprodigital/webcrawler/consumer/ResultPrinter.java
@@ -1,6 +1,7 @@
 
 package org.wiprodigital.webcrawler.consumer;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.wiprodigital.webcrawler.model.WebPage;
@@ -13,11 +14,22 @@ public class ResultPrinter implements Consumer<WebPage> {
 		System.out.println("Page URL: " + webPage.getUrl());
 
 		if (webPage.getErrorString() == null) {
-			System.out.println("Internal Links" + webPage.getInternalLinks());
-			System.out.println("External Links" + webPage.getExternalLinks());
-			System.out.println("Static Content" + webPage.getStaticContent());
+			System.out.println("Internal Links");
+			printList(webPage.getInternalLinks());
+			System.out.println("External Links");
+			printList(webPage.getExternalLinks());
+			System.out.println("Static Content");
+			printList(webPage.getStaticContent());
 		} else {
 			System.out.println("Error" + webPage.getErrorString());
+		}
+
+	}
+
+	private void printList(List<String> links) {
+		for (String link : links) {
+			System.out.println("--" + link);
+
 		}
 
 	}
